7b0a1849bf48c63f0006d6e30b1aec5e367ba255
xerexcoded <arnavbanerjee61@gmail.com>
2023-03-15 22:55:03 +0530

    sprouting

---
+++ src/main/java/site/srht/xerexcoded/content/calender/Application.java
@@ -0,0 +1,18 @@
+package site.srht.xerexcoded.content.calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+ SpringApplication.run(Application.class, args);
+
+	}
+
+}
---
+++ src/test/java/site/srht/xerexcoded/content/calender/ContentCalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package site.srht.xerexcoded.content.calender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
